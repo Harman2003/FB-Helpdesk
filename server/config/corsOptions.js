747c76f36926b32f0730c23b923e786761f2915c
Harman Singh <singhharmandhindsa@gmail.com>
2024-03-11 01:12:33 +0530

    fix: cors error

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "https://0a8e-2401-4900-80af-6adb-8060-2dcb-86c1-bf7d.ngrok-free.app",
     "http://localhost:5173",
-    "https://helpdeskfb.vercel.app/",
+    "https://helpdeskfb.vercel.app",
   ],
   credentials: true,
 };
