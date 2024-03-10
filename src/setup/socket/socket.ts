044bba779bb7935dcecbdb3610375c3c21d579c7
Harman Singh <singhharmandhindsa@gmail.com>
2024-03-11 01:51:21 +0530

    fix:modified socket url

--- src/setup/socket/socket.ts
+++ src/setup/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = "https://fb-helpdesk-server-dev-hajj.1.us-1.fl0.io";
 export const socket = io(URL, {
     autoConnect: false,
 });
